74bc721fa008012b9b28a1d7af8e341b1b261eb4
Tim Suellentrop <tim@timsuellentrop.com>
2017-11-20 12:27:53 -0500

    set flamingo to allow editors to edit/see inbound messages

--- abcmaine/functions.php
+++ abcmaine/functions.php
@@ -17,4 +17,21 @@
   require_once(get_stylesheet_directory().'/custom/tribe_events.php');
   //require_once(get_stylesheet_directory().'/custom/bottom-CTA.php');
 
+
+  add_filter( 'flamingo_map_meta_cap', 'reach_flamingo_map_meta_cap' );
+
+  function reach_flamingo_map_meta_cap( $meta_caps ) {
+  	$meta_caps = array_merge( $meta_caps, array(
+
+  		'flamingo_edit_inbound_messages' => 'edit_pages', // allow on admin sidebar  - works, but cant do anything.
+      'flamingo_edit_inbound_message' => 'edit_posts', // allow editor to see it
+  		'flamingo_delete_inbound_message' => 'delete_pages',
+  		'flamingo_delete_inbound_messages' => 'delete_pages',
+  		'flamingo_spam_inbound_message' => 'delete_pages',
+  		'flamingo_unspam_inbound_message' => 'delete_pages',
+
+  	) );
+
+  	return $meta_caps;
+  }
 ?>
